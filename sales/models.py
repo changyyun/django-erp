b9040256ac9083103ff850d6cdde6ecc19736543
Chang Yun <47656782+changyyun@users.noreply.github.com>
2024-10-13 12:55:29 -0700

    create basic customer model

--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    date_created = models.DateTimeField('date created')
+
+    def __str__(self):
+        return f"{self.name}, {self.date_created}"
